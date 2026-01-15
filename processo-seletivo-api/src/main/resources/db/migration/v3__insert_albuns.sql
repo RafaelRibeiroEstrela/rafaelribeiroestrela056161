c19d90538b9a6104213408cfd71b59dc2e92aded
Rafael Ribeiro Estrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2026-01-15 14:32:26 -0400

    Create v3__insert_albuns.sql

---
+++ processo-seletivo-api/src/main/resources/db/migration/v3__insert_albuns.sql
@@ -0,0 +1,26 @@
+insert into tb_albuns(nome)
+values ('Harakiri');
+insert into tb_albuns(nome)
+values ('Black Blooms');
+insert into tb_albuns(nome)
+values ('The Rough Dog');
+insert into tb_albuns(nome)
+values ('The Rising Tied');
+insert into tb_albuns(nome)
+values ('Post Traumatic');
+insert into tb_albuns(nome)
+values ('Post Traumatic EP');
+insert into tb_albuns(nome)
+values ('Where’d You Go');
+insert into tb_albuns(nome)
+values ('Bem Sertanejo');
+insert into tb_albuns(nome)
+values ('Bem Sertanejo - O Show (Ao Vivo)');
+insert into tb_albuns(nome)
+values ('Bem Sertanejo - (1ª Temporada) - EP');
+insert into tb_albuns(nome)
+values ('Use Your Illusion I');
+insert into tb_albuns(nome)
+values ('Use Your Illusion II');
+insert into tb_albuns(nome)
+values ('Greatest Hits');
